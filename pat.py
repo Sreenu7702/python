82e91691fc020acb12b576370cde3b04c655f19c
Sreenu7702 <t.sreenu2003@gmail.com>
2025-09-15 21:55:47 +0530

    Add files via upload

---
+++ pat.py
@@ -0,0 +1,14 @@
+#square pattern
+n=5
+for i in range(n):
+    for j in range(n):
+        print("*",end=" ")
+    print()
+
+#right angle triangle
+n=5
+for i in range(n):
+    for j in range(i+1):
+        print("*",end=" ")
+    print()
+    
